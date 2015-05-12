3049922684f42f29b6ba7753c43525dcb8855d28
Toby Schrapel <toby@tobyschrapel.com>
2015-05-12 08:06:26 -0500

    Downgrade php version for WPEngine

--- wp-content/themes/intranet-lawcom/lib/init.php
+++ wp-content/themes/intranet-lawcom/lib/init.php
@@ -14,14 +14,14 @@ function setup() {
 
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
-  register_nav_menus([
+  register_nav_menus(array(
     'primary_navigation' => __('Primary Navigation', 'sage'),
     'sidebar_navigation' => __('Sidebar Navigation', 'sage')
-  ]);
+  ));
 
   // Add HTML5 markup for captions
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-  add_theme_support('html5', ['caption', 'comment-form', 'comment-list']);
+  add_theme_support('html5', array('caption', 'comment-form', 'comment-list'));
 
   // Tell the TinyMCE editor to use a custom stylesheet
   add_editor_style(trailingslashit(get_template_directory_uri()) . "global2.css");
