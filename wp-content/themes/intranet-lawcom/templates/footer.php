f8d2f7acfcb95c37b264f25b36884f6f59eaebd4
Toby Schrapel <toby@tobyschrapel.com>
2015-05-11 21:02:06 -0500

    Add intranet theme

---
+++ wp-content/themes/intranet-lawcom/templates/footer.php
@@ -0,0 +1,6 @@
+<!-- footer -->
+<div class="footer">
+<p>&copy; <a href="copydisc.htm">Crown Copyright and Disclaimer</a><br />&nbsp;&nbsp;&nbsp;&nbsp;Last updated: 23 April 2015</p>
+
+</div>
+</div>
---
+++ wp-content/themes/intranet-lawcom/templates/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head><!-- PageID 1 - published by RedDot 7.5 - 7.5.2.17 - 24533 -->
+    <!--<title>Law Commission: Latest messages</title>
+    <meta name="DC.title" content="Law Commission: Latest messages">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="Keywords" content="">
+    <meta name="DC.subject.keyword" content="">
+    <meta name="Description" content="">
+    <meta name="DC.description" content="">
+    <meta name="DC.creator" content="Law Commission">
+    <meta name="DC.contributor" content="">
+    <meta name="DC.identifier" content="index.htm">
+    <meta name="DC.date.created" content="2005-04-26">
+    <meta name="DC.date.modified" content="2015-04-23">
+    <meta name="DC.publisher" content="Law Commission">
+    <meta name="eGMS.subject.category" scheme="GCL" content="Crime, Law, Justice and Rights">
+    <meta name="DC.format" content="Text/HTML">
+    <meta name="DC.language" content="eng">
+    <meta name="DC.coverage" content="England, Wales">
+    <meta name="DC.rights.copyright" content="Crown Copyright">-->
+    <!-- stylesheet selector to force ie5.5 browsers to use iestyle.css -->
+    <?php wp_head(); ?>
+  </head>
