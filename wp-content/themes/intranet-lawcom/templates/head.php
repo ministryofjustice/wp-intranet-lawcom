96adeac451b1c709f19b69f7bda904adb04dca9f
Toby Schrapel <toby@tobyschrapel.com>
2015-05-13 08:33:33 -0500

    Setup DC tags

--- wp-content/themes/intranet-lawcom/templates/head.php
+++ wp-content/themes/intranet-lawcom/templates/head.php
@@ -1,8 +1,8 @@
+<?php use Roots\Sage\Titles; global $post; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head><!-- PageID 1 - published by RedDot 7.5 - 7.5.2.17 - 24533 -->
-    <!--<title>Law Commission: Latest messages</title>
-    <meta name="DC.title" content="Law Commission: Latest messages">
+  <head>
+    <meta name="DC.title" content="Law Commission: <?= Titles\title(); ?>">
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="Keywords" content="">
     <meta name="DC.subject.keyword" content="">
@@ -10,15 +10,14 @@
     <meta name="DC.description" content="">
     <meta name="DC.creator" content="Law Commission">
     <meta name="DC.contributor" content="">
-    <meta name="DC.identifier" content="index.htm">
-    <meta name="DC.date.created" content="2005-04-26">
-    <meta name="DC.date.modified" content="2015-04-23">
+    <meta name="DC.identifier" content="<?= $post->post_name; ?>">
+    <meta name="DC.date.created" content="<?= get_the_date( 'Y-m-d', get_the_ID() ); ?>">
+    <meta name="DC.date.modified" content="<?php echo get_the_modified_date( 'Y-m-d' ); ?>">
     <meta name="DC.publisher" content="Law Commission">
     <meta name="eGMS.subject.category" scheme="GCL" content="Crime, Law, Justice and Rights">
     <meta name="DC.format" content="Text/HTML">
     <meta name="DC.language" content="eng">
     <meta name="DC.coverage" content="England, Wales">
-    <meta name="DC.rights.copyright" content="Crown Copyright">-->
-    <!-- stylesheet selector to force ie5.5 browsers to use iestyle.css -->
+    <meta name="DC.rights.copyright" content="Crown Copyright">
     <?php wp_head(); ?>
   </head>
