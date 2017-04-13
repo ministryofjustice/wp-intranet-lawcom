5985c59440947683bfeab05c782258775fc874f9
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-04-13 15:58:04 +0100

    Remove support for soil-nav-walker as it was breaking menu styling. Also remove relative-urls since they’ve not been used so far, so it’ll keep the DB consistent.

--- web/app/themes/intranet-lawcom/lib/config.php
+++ web/app/themes/intranet-lawcom/lib/config.php
@@ -8,7 +8,5 @@ use Roots\Sage\ConditionalTagCheck;
  * Enable theme features
  */
 add_theme_support('soil-clean-up');         // Enable clean up from Soil
-add_theme_support('soil-nav-walker');       // Enable cleaner nav walker from Soil
-add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
 add_theme_support('soil-nice-search');      // Enable nice search from Soil
 add_theme_support('soil-disable-trackbacks'); // Disable trackbacks/pingbacks
